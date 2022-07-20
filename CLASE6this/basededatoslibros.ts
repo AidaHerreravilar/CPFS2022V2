b345e607f205dad0e1b0f625afcb3f95be77d2df
AidaHerreravilar <aidaherreravilar383@gmail.com>
2022-07-20 17:25:30 -0300

    ejercicio clase 6

---
+++ CLASE6this/basededatoslibros.ts
@@ -0,0 +1,16 @@
+export class libro {
+    private nombre: string;
+    private totalPaginas: number;
+    constructor(nombre: string, paginas: number) {
+        this.nombre = nombre;
+        this.totalPaginas = paginas
+    }
+        public getNombre() : string{
+            return this.nombre;
+            }
+setNombre(nombre:string){
+    this.nombre=nombre;
+}
+            }
+        
+    
