42a3b4b40f8386051c88560fa5ca34f2bf6fba07
AidaHerreravilar <aidaherreravilar383@gmail.com>
2022-08-20 14:21:50 -0300

    resolución ejercicio clase 6 libro ts

---
+++ CLASE6this/libro.ts
@@ -0,0 +1,10 @@
+import * as ReadlineSync from  "readline-sync";
+let libroInsertado = "Ingrese el numero en el cual se encuentra el libro que desea insertar";
+let libroConsultado = "Ingrese el libro que desea consultar";
+let libroModificado = "Ingrese el numero en el cual se encuenttra el libro que desea modificar";
+
+class libro {
+    nombre: string;
+    autor: string;
+    pagina: number; 
+}
